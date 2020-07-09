8a92afdc15d5fc0c3fadcd6a6ba3e5dc4f519aa5
shaffat <shaffatbd@gmail.com>
2020-07-09 08:24:44 +0600

    .net starting Project

---
+++ FirstApp/FirstApp/hello.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Bangladesh");
+            Console.ReadLine();
+        }
+    }
+}
